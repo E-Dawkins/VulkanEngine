02a7add0d3d172564d5dfbde5846077dfa624e51
E-Dawkins <ethandawkins@hotmail.co.uk>
2024-01-11 14:03:02 +1100

    Updated compile.bat to compile all shader files

--- VulkanEngine/shaders/compile.bat
+++ VulkanEngine/shaders/compile.bat
@@ -1,3 +1,9 @@
-D:/Libraries/VulkanSDK_1_3_268/Bin/glslc.exe shader.vert -o vert.spv
-D:/Libraries/VulkanSDK_1_3_268/Bin/glslc.exe shader.frag -o frag.spv
+for /r %%i in (*.vert) do (
+  D:/Libraries/VulkanSDK_1_3_268/Bin/glslc.exe %%i -o %%i.spv
+)
+
+for /r %%i in (*.frag) do (
+  D:/Libraries/VulkanSDK_1_3_268/Bin/glslc.exe %%i -o %%i.spv
+)
+
 pause
